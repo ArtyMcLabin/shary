a153955cc4192abc5d34b0a9eb8a76c39551b5b3
Arty Party <arty.mclabin@gmail.com>
2017-01-14 20:06:27 +0200

    fixed "get" issue with some cygwins

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,11 @@ int main(int argc, char* argv[])
     switch(command)
     {
         case Command::get:
-            system(("nc -l -p "+port+" > data.shary.zip && unzip data.shary.zip && rm data.shary.zip").c_str());
+            #ifdef __CYGWIN__
+                system(("nc -l "+port+" > data.shary.zip && unzip data.shary.zip && rm data.shary.zip").c_str());
+            #else
+                system(("nc -l -p "+port+" > data.shary.zip && unzip data.shary.zip && rm data.shary.zip").c_str());
+            #endif
             break;
         case Command::give:
             {
